7255111f4e5b8bad2599141185b725f685e2c5ac
Kishore P <kidkrrish3@gmail.com>
2024-10-13 19:47:50 +0530

    new commit 1

--- server/index.js
+++ server/index.js
@@ -1,27 +1,42 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const { eventModel, oviyamModel, sirukadhaiModel, puthagamModel, vasanamModel, vidukadhaiModel, naatkurippuModel } = require('./mongo');
-const imgdown = require('image-downloader');
-const multer = require('multer');
-const fs = require('fs');
+const {
+  eventModel,
+  oviyamModel,
+  sirukadhaiModel,
+  puthagamModel,
+  vasanamModel,
+  vidukadhaiModel,
+  naatkurippuModel,
+} = require("./mongo");
+const imgdown = require("image-downloader");
+const multer = require("multer");
+const fs = require("fs");
 
 app.use(express.json());
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-}));
+app.use(
+  cors({
+    origin: "https://kanaiyazhi-admin-panel.onrender.com",
+  })
+);
 
 app.get("/", (req, res) => {
-  res.send("hi da");
+  res.send("Vanakkam da mapla!");
 });
 
 app.post("/kavithai", async (req, res) => {
   const { title, subtitle, date, authorName, content, addedPhotos } = req.body;
-  console.log({ title, subtitle, date, authorName, content, addedPhotos });     
+  console.log({ title, subtitle, date, authorName, content, addedPhotos });
   const eveDoc = await eventModel.create({
-    title, subtitle, date, authorName, content, photos:addedPhotos
+    title,
+    subtitle,
+    date,
+    authorName,
+    content,
+    photos: addedPhotos,
   });
   res.send(eveDoc);
 });
@@ -29,7 +44,12 @@ app.post("/kavithai", async (req, res) => {
 app.post("/oviyam", async (req, res) => {
   const { title, subtitle, date, authorName, content, addedPhotos } = req.body;
   const eveDoc = await oviyamModel.create({
-    title, subtitle, date, authorName, content, photos:addedPhotos
+    title,
+    subtitle,
+    date,
+    authorName,
+    content,
+    photos: addedPhotos,
   });
   res.send(eveDoc);
 });
@@ -37,16 +57,26 @@ app.post("/oviyam", async (req, res) => {
 app.post("/sirukadhai", async (req, res) => {
   const { title, subtitle, date, authorName, content, addedPhotos } = req.body;
   const eveDoc = await sirukadhaiModel.create({
-    title, subtitle, date, authorName, content, photos:addedPhotos
+    title,
+    subtitle,
+    date,
+    authorName,
+    content,
+    photos: addedPhotos,
   });
   res.send(eveDoc);
 });
 
 app.post("/puthaga-vimarsanam", async (req, res) => {
   const { title, subtitle, date, authorName, content, addedPhotos } = req.body;
-  console.log({ title, subtitle, date, authorName, content, addedPhotos });     
+  console.log({ title, subtitle, date, authorName, content, addedPhotos });
   const eveDoc = await puthagamModel.create({
-    title, subtitle, date, authorName, content, photos:addedPhotos
+    title,
+    subtitle,
+    date,
+    authorName,
+    content,
+    photos: addedPhotos,
   });
   res.send(eveDoc);
 });
@@ -54,7 +84,12 @@ app.post("/puthaga-vimarsanam", async (req, res) => {
 app.post("/vasanam", async (req, res) => {
   const { title, subtitle, date, authorName, content, addedPhotos } = req.body;
   const eveDoc = await vasanamModel.create({
-    title, subtitle, date, authorName, content, photos:addedPhotos
+    title,
+    subtitle,
+    date,
+    authorName,
+    content,
+    photos: addedPhotos,
   });
   res.send(eveDoc);
 });
@@ -62,7 +97,12 @@ app.post("/vasanam", async (req, res) => {
 app.post("/vidukadhai", async (req, res) => {
   const { title, subtitle, date, authorName, content, addedPhotos } = req.body;
   const eveDoc = await vidukadhaiModel.create({
-    title, subtitle, date, authorName, content, photos:addedPhotos
+    title,
+    subtitle,
+    date,
+    authorName,
+    content,
+    photos: addedPhotos,
   });
   res.send(eveDoc);
 });
@@ -70,7 +110,12 @@ app.post("/vidukadhai", async (req, res) => {
 app.post("/naatkurippu", async (req, res) => {
   const { title, subtitle, date, authorName, content, addedPhotos } = req.body;
   const eveDoc = await naatkurippuModel.create({
-    title, subtitle, date, authorName, content, photos:addedPhotos
+    title,
+    subtitle,
+    date,
+    authorName,
+    content,
+    photos: addedPhotos,
   });
   res.send(eveDoc);
 });
@@ -81,16 +126,16 @@ app.post("/upload-by-link", async (req, res) => {
   const newName = "photo" + Date.now() + ".jpg";
   await imgdown.image({
     url: validUrl.href,
-    dest: __dirname + '/uploads/' + newName
+    dest: __dirname + "/uploads/" + newName,
   });
   res.json(newName);
 });
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
-app.post("/uploads", upload.array('photo', 100),(req, res) => {
+app.post("/uploads", upload.array("photo", 100), (req, res) => {
   const upfiles = [];
-  req.files.forEach(file => {
+  req.files.forEach((file) => {
     const { path, originalname } = file;
     const orgparts = originalname.split(".");
     const ext = orgparts[orgparts.length - 1];
@@ -104,4 +149,3 @@ app.post("/uploads", upload.array('photo', 100),(req, res) => {
 app.listen(5000, () => {
   console.log("server is listening...");
 });
-
--- server/mongo.js
+++ server/mongo.js
@@ -1,82 +1,95 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
-mongoose.connect("mongodb://localhost:27017").then(()=>{
-    console.log('database connected...');
-}).catch((err)=>{
-    console.log(err)
-})
+mongoose
+  .connect(
+    "mongodb+srv://kidkrrish3:kidkrrish3@tmcluster.wznin.mongodb.net/?retryWrites=true&w=majority&appName=TMCluster"
+  )
+  .then(() => {
+    console.log("database connected...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const eventSchema=new mongoose.Schema({
-    title: String,
-    subtitle: String,
-    authorName: String,
-    date: String,
-    content: String,
-    photos: [String],
-})
+const eventSchema = new mongoose.Schema({
+  title: String,
+  subtitle: String,
+  authorName: String,
+  date: String,
+  content: String,
+  photos: [String],
+});
 
 const oviyamSchema = new mongoose.Schema({
-    title: String,
-    subtitle: String,
-    date: String,
-    authorName: String,
-    content: String,
-    photos: [String],
-})
+  title: String,
+  subtitle: String,
+  date: String,
+  authorName: String,
+  content: String,
+  photos: [String],
+});
 
 const sirukadhaiSchema = new mongoose.Schema({
-    title: String,
-    subtitle: String,
-    date: String,
-    authorName: String,
-    content: String,
-    photos: [String],
-})
+  title: String,
+  subtitle: String,
+  date: String,
+  authorName: String,
+  content: String,
+  photos: [String],
+});
 
-const puthagamSchema=new mongoose.Schema({
-    title: String,
-    subtitle: String,
-    authorName: String,
-    date: String,
-    content: String,
-    photos: [String],
-})
+const puthagamSchema = new mongoose.Schema({
+  title: String,
+  subtitle: String,
+  authorName: String,
+  date: String,
+  content: String,
+  photos: [String],
+});
 
 const vasanamSchema = new mongoose.Schema({
-    title: String,
-    subtitle: String,
-    date: String,
-    authorName: String,
-    content: String,
-    photos: [String],
-})
+  title: String,
+  subtitle: String,
+  date: String,
+  authorName: String,
+  content: String,
+  photos: [String],
+});
 
 const vidukadhaiSchema = new mongoose.Schema({
-    title: String,
-    subtitle: String,
-    date: String,
-    authorName: String,
-    content: String,
-    photos: [String],
-})
+  title: String,
+  subtitle: String,
+  date: String,
+  authorName: String,
+  content: String,
+  photos: [String],
+});
 
 const naatkurippuSchema = new mongoose.Schema({
-    title: String,
-    subtitle: String,
-    date: String,
-    authorName: String,
-    content: String,
-    photos: [String],
-})
+  title: String,
+  subtitle: String,
+  date: String,
+  authorName: String,
+  content: String,
+  photos: [String],
+});
 
-const eventModel=mongoose.model('kavithai',eventSchema);
-const oviyamModel = mongoose.model('oviyam', oviyamSchema);
-const sirukadhaiModel = mongoose.model('sirukadhai', sirukadhaiSchema);
-const puthagamModel = mongoose.model('puthagam', puthagamSchema);
-const vasanamModel = mongoose.model('vasanam', vasanamSchema);
-const vidukadhaiModel = mongoose.model('vidukadhai', vidukadhaiSchema);
-const naatkurippuModel = mongoose.model('naatkurippu', naatkurippuSchema);
+const eventModel = mongoose.model("kavithai", eventSchema);
+const oviyamModel = mongoose.model("oviyam", oviyamSchema);
+const sirukadhaiModel = mongoose.model("sirukadhai", sirukadhaiSchema);
+const puthagamModel = mongoose.model("puthagam", puthagamSchema);
+const vasanamModel = mongoose.model("vasanam", vasanamSchema);
+const vidukadhaiModel = mongoose.model("vidukadhai", vidukadhaiSchema);
+const naatkurippuModel = mongoose.model("naatkurippu", naatkurippuSchema);
 
-module.exports = { eventModel, oviyamModel, sirukadhaiModel, puthagamModel, vasanamModel, vidukadhaiModel, naatkurippuModel};
+module.exports = {
+  eventModel,
+  oviyamModel,
+  sirukadhaiModel,
+  puthagamModel,
+  vasanamModel,
+  vidukadhaiModel,
+  naatkurippuModel,
+};
