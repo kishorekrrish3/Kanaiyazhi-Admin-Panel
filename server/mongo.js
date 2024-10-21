796d93c645a758d44c28065e68d69e43214ea71a
Kishore P <kidkrrish3@gmail.com>
2024-10-21 21:05:08 +0530

    added https

--- server/mongo.js
+++ server/mongo.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect("mongodb://localhost:27017/")
+  .connect("https://mongodb://localhost:27017/")
   .then(() => {
     console.log("database connected...");
   })
